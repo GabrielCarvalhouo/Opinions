0a531be44aaf6936dc8fb954a68eef3c0a410971
GabrielCarvalhouo <gabrielvasconcellos76@gmail.com>
2023-09-16 13:32:26 -0300

    first commit

---
+++ src/main/java/com/example/opnions/OpnionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.opnions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpnionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpnionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/opnions/OpnionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opnions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpnionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
